5abeb5aaad187e90ce3eaa6368d87ee330beea5b
shepel04 <95418153+shepel04@users.noreply.github.com>
2024-06-02 13:37:29 +0300

    1

--- Assets/Scripts/Tank/UserInterface/FirePower.cs
+++ Assets/Scripts/Tank/UserInterface/FirePower.cs
@@ -1,6 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Recorder;
+//using UnityEditor.Recorder;
 using UnityEngine;
 using UnityEngine.PlayerLoop;
 using UnityEngine.UI;
