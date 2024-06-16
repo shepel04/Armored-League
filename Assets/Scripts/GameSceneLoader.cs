b9058a44e11e5c0e0058d0b07403cfb1c022719b
shepel04 <95418153+shepel04@users.noreply.github.com>
2024-06-16 17:14:27 +0300

    final

--- Assets/Scripts/GameSceneLoader.cs
+++ Assets/Scripts/GameSceneLoader.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameSceneLoader : MonoBehaviour
 {
     public void LoadSceneByNumber(int sceneNumber)
     {
-        
+        Debug.Log("Scene loading");
+        SceneManager.LoadScene(sceneNumber);
     }
 }
--- Assets/Scripts/InGameInputHandler.cs
+++ Assets/Scripts/InGameInputHandler.cs
@@ -3,28 +3,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class InGameInputHandler : MonoBehaviour
 {
     public GameObject MenuUI;
+    public GameObject HUD;
 
     private bool _isMenuActive = false;
 
-    private void OnEnable()
+    private void Update()
     {
-        var playerInput = GetComponent<PlayerInput>();
-        playerInput.actions["InGameMenuCall"].performed += OnOpenMenu;
+        if (Input.GetButtonDown("Cancel"))
+        {
+            OnOpenMenu();
+        }
     }
 
-    private void OnDisable()
-    {
-        var playerInput = GetComponent<PlayerInput>();
-        playerInput.actions["InGameMenuCall"].performed -= OnOpenMenu;
-    }
-
-    private void OnOpenMenu(InputAction.CallbackContext context)
+    private void OnOpenMenu()
     {
         MenuUI.SetActive(!MenuUI.activeSelf);
+        HUD.SetActive(!HUD.activeSelf);
     }
 }
